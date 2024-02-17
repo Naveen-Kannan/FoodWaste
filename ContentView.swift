93ea774a4bb5f083d3e3abca25808d7b8488ec75
Peter Zhang <pzhang04@stanford.edu>
2024-02-17 13:02:22 -0800

    updated with text and delete

--- ContentView.swift
+++ ContentView.swift
@@ -1,77 +1,186 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab = 0
+    @State private var isCameraPresented = false
+    @State private var image: Image? = nil
+    @State private var foodItems: [FoodItem] = []
+    @State private var newItemName: String = ""
+    @State private var newItemDate: String = ""
+    @State private var newItemExpirationDate: String = ""
+
     var body: some View {
+        TabView(selection: $selectedTab) {
+            homeScreen
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+                .tag(0)
+
+            rawInventoryScreen
+                .tabItem {
+                    Image(systemName: "leaf.fill")
+                    Text("Raw Inventory")
+                }
+                .tag(1)
+
+            Color.red
+                .tabItem {
+                    Image(systemName: "book.fill")
+                    Text("Recipes")
+                }
+                .tag(2)
+
+            Color.gray
+                .tabItem {
+                    Image(systemName: "trash.fill")
+                    Text("Waste Reduction")
+                }
+                .tag(3)
+
+            Color.blue
+                .tabItem {
+                    Image(systemName: "message.fill")
+                    Text("Chat")
+                }
+                .tag(4)
+        }
+    }
+
+    var homeScreen: some View {
         GeometryReader { geometry in
             VStack {
                 Spacer().frame(height: geometry.size.height * 0.05)
                 HStack {
                     Spacer()
-                    Text("Centered View")
-                        .padding()
-                        .frame(width: geometry.size.width * 0.90, height: geometry.size.height * 0.80)
-                        .background(Color.blue.opacity(0.5))
-                        .cornerRadius(10)
+                    VStack {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: geometry.size.width * 0.90, height: geometry.size.height * 0.80)
+                            .cornerRadius(10)
+                        Button(action: {
+                            isCameraPresented = true
+                        }) {
+                            Image(systemName: "camera")
+                                .font(.largeTitle)
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .clipShape(Circle())
+                        }
+                    }
                     Spacer()
                 }
-                Spacer().frame(height: geometry.size.height * 0.03)
-                
-                // Red rectangle with aligned images and text
-                HStack {
-                    Spacer() // Add a spacer for the left border
-                    VStack {
-                        Image(systemName: "leaf.fill")
-                            .font(.system(size: 28))
-                            .foregroundColor(.white)
-                        Spacer().frame(height: 6) // Adjust the space between this image and text
-                        Text("Raw\nInventory")
-                            .font(.system(size: 14))
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(.white)
-                    }
-                    Spacer()
-                    VStack {
-                        Image(systemName: "book.fill")
-                            .font(.system(size: 28))
-                            .foregroundColor(.white)
-                        Spacer().frame(height: 18) // Adjust the space between this image and text
-                        Text("Recipes")
-                            .font(.system(size: 14))
-                            .foregroundColor(.white)
-                    }
-                    Spacer()
-                    VStack {
-                        Image(systemName: "trash.fill")
-                            .font(.system(size: 28))
-                            .foregroundColor(.white)
-                        Spacer().frame(height: 10) // Adjust the space between this image and text
-                        Text("Waste\nReduction")
-                            .font(.system(size: 14))
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(.white)
-                    }
-                    Spacer()
-                    VStack {
-                        Image(systemName: "message.fill")
-                            .font(.system(size: 28))
-                            .foregroundColor(.white)
-                        Spacer().frame(height: 18) // Adjust the space between this image and text
-                        Text("Chat")
-                            .font(.system(size: 14))
-                            .foregroundColor(.white)
-                    }
-                    Spacer() // Add a spacer for the right border
-                }
-                .frame(maxWidth: .infinity, maxHeight: geometry.size.height * 0.2)
-                .background(Color.red)
+                Spacer() // Add a spacer to fill the remaining space
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color.white)
+            .sheet(isPresented: $isCameraPresented) {
+                ImagePicker(sourceType: .camera, selectedImage: $image)
+            }
         }
-        .edgesIgnoringSafeArea(.all)
+    }
+
+    var rawInventoryScreen: some View {
+        VStack {
+            TextField("Item Name", text: $newItemName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            TextField("Date", text: $newItemDate)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            TextField("Expiration Date", text: $newItemExpirationDate)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Button("Add Food Item") {
+                let newItem = FoodItem(itemName: newItemName, date: newItemDate, expirationDate: newItemExpirationDate)
+                foodItems.append(newItem)
+                newItemName = ""
+                newItemDate = ""
+                newItemExpirationDate = ""
+                hideKeyboard()
+            }
+            .padding()
+            .background(Color.green)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+
+            List {
+                ForEach(foodItems) { item in
+                    VStack(alignment: .leading) {
+                        Text(item.itemName)
+                            .font(.headline)
+                        Text(item.date)
+                        Text(item.expirationDate)
+                    }
+                }
+                .onDelete(perform: deleteFoodItems)
+            }
+            .toolbar {
+                EditButton()
+            }
+        }
+        .onTapGesture {
+            hideKeyboard()
+        }
+    }
+
+    func deleteFoodItems(at offsets: IndexSet) {
+        foodItems.remove(atOffsets: offsets)
     }
 }
 
-// Preview {
-//     ContentView()
-// }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    var sourceType: UIImagePickerController.SourceType
+    @Binding var selectedImage: Image?
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = Image(uiImage: image)
+            }
+
+            picker.dismiss(animated: true)
+        }
+    }
+}
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+struct FoodItem: Identifiable {
+    var id = UUID()
+    var itemName: String
+    var date: String
+    var expirationDate: String
+}
