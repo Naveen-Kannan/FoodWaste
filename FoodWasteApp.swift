04d9c0c5a298b66477d63d1489a1aecac5361993
piggybank42130 <pzhang1124@gmail.com>
2024-02-16 21:06:13 -0800

    Initial commit

---
+++ FoodWasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodWasteApp.swift
+//  FoodWaste
+//
+//  Created by Peter Zhang on 2024/2/16.
+//
+
+import SwiftUI
+
+@main
+struct FoodWasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
